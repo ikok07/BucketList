3e93e3c53a8d842588520afa04a09654cc0525fa
Kaloyan Stefanov <kokmarok@gmail.com>
2024-11-07 11:22:27 +0200

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Kok on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
